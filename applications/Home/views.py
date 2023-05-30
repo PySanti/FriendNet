934f654843fd1c2b71d54e1432ce080586b71996
santiago <santiagodeandrade4@gmail.com>
2023-05-30 08:21:12 -0400

    Implementacion de Model Home y HomeView

--- applications/Home/views.py
+++ applications/Home/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.views.generic import (
+    TemplateView
+)
+from .models import Home
 # Create your views here.
+class HomeView(TemplateView):
+    template_name = 'Home/home_view.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['home_data'] = Home.objects.get(id=2)
+        return context
