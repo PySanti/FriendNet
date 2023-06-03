b17a5e0ce530a710db95e71dbed46d98bf12a48a
santiago <santiagodeandrade4@gmail.com>
2023-06-03 16:22:59 -0400

    Modificacion en carga de secrets

--- FriendNet/settings/local.py
+++ FriendNet/settings/local.py
@@ -4,6 +4,23 @@ from ..tools import read_secret_data
 
 DEBUG = True
 ALLOWED_HOSTS = []
-DATABASES, SECRET_KEY = read_secret_data('local', SECRET_FILE_PATH)
+SECRETS = read_secret_data('local', SECRET_FILE_PATH)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': SECRETS['LOCAL_DB'],
+        'USER': SECRETS['USER'],
+        'PASSWORD': SECRETS['PASSWORD'],
+        'HOST': SECRETS['HOST'],
+        'PORT': SECRETS['PORT'],
+    }
+}
+SECRET_KEY = SECRETS['KEY']
 MEDIA_URL = 'media/'  
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')  
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = SECRETS['EMAIL_USER']
+EMAIL_HOST_PASSWORD = SECRETS['EMAIL_PASSWORD']
+EMAIL_PORT = 587
--- FriendNet/tools.py
+++ FriendNet/tools.py
@@ -4,15 +4,4 @@ from json import load
 def read_secret_data(env, secret_filename):
     with open(secret_filename, 'r') as f:
         info = load(f)
-        SECRET_KEY = info['KEY']
-        DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.postgresql_psycopg2',
-                'NAME': info['LOCAL_DB'] if env == 'local' else info['PROD_DB'] ,
-                'USER': info['USER'],
-                'PASSWORD': info['PASSWORD'],
-                'HOST': info['HOST'],
-                'PORT': info['PORT'],
-            }
-        }
-    return DATABASES, SECRET_KEY
+    return info
