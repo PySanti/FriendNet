16fbdbe3798de338541ef1f8d8b600279e0a4dff
santiago <santiagodeandrade4@gmail.com>
2023-06-03 18:05:36 -0400

    Limpieza de DB

--- applications/Home/views.py
+++ applications/Home/views.py
@@ -7,5 +7,5 @@ class HomeView(TemplateView):
     template_name = 'Home/home_view.html'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['home_data'] = Home.objects.get(id=2)
+        context['home_data'] = Home.objects.get(id=1)
         return context
