a95b6edee4a79fe9984f5754e6335f15722d113a
santiago <santiagodeandrade4@gmail.com>
2023-06-20 11:04:02 -0400

    Implementacion de estilos base para home

--- applications/Home/forms.py
+++ applications/Home/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 
 class MessagesForm(forms.Form):
-    msg = forms.CharField(required=False, max_length=50, widget=forms.TextInput(attrs={
-        'placeholder' : 'Ingresa el mensaje'
+    msg = forms.CharField(label="",required=False, max_length=50, widget=forms.TextInput(attrs={
+        'placeholder' : 'Ingresa un mensaje',
+        'class' : 'django-msg-input'
     }))
 
 class StatusForm(forms.Form):
