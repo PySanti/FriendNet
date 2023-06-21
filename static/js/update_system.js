c42dd19df219ca3a471eae295cec43339048c489
santiago <santiagodeandrade4@gmail.com>
2023-06-21 12:28:00 -0400

    Commit post-estilos de la parte de actualizacion, requere refactor

--- static/js/update_system.js
+++ static/js/update_system.js
@@ -2,19 +2,22 @@ FORM_CLASSNAME = "detail-form"
 DETAILLIST_CLASSNAME = "detail-list"
 UPDATE_BTN_CLASSNAME = "update-btn"
 SECTION_CLASSNAME = "detail-container"
+DETAIL_IMG_PREVIEW = "img-detail-preview"
 function setUpdatingFunction(form_classname, detailList_classname, updateBtn_classname, section_classname){
     /*
         Funcion creada para eliminar inicialmente el formulario de actualizacion del template,
         por otro lado, configurar los elementos para, al presionar el update-btn,
         surga el formulario
     */
+    detailImg =  document.getElementById(DETAIL_IMG_PREVIEW)
     section = document.getElementsByClassName(section_classname)[0]
     form = document.getElementsByClassName(form_classname)[0]
     detailList = document.getElementsByClassName(detailList_classname)[0]
     updateBtn = document.getElementsByClassName(updateBtn_classname)[0]
-    form.remove()
     updateBtn.addEventListener("click", ()=>{
+        form.classList.add('form-activated')
         section.appendChild(form)
+        detailImg.remove()
         detailList.remove()
         updateBtn.remove()
     })
