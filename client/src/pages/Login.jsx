f844bc4fb6d39d487101de80f86f604083737064
santiago <santiagodeandrade4@gmail.com>
2023-06-24 22:55:24 -0400

    Base de Login.jsx creada

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,14 +1,54 @@
+import { useForm } from "react-hook-form"
 import { Header } from "../components/Header"
-import {BACKEND_URL} from "../main.jsx"
 
 
 export function Login() {
+    const {register, handleSubmit, formState : {errors}} = useForm()
+    const onSubmit = handleSubmit((data)=>{
+    })
     return (
         <>
             <Header/>
-            <form action={BACKEND_URL+"/login"} method="POST">
-                <input type="text"></input>
-                <input type="password"></input>
+            <form onSubmit={onSubmit} method="POST">
+                {errors.username && <p>{errors.username.message}</p>}
+                <label>
+                    Usuario:
+                    <input 
+                        maxLength={15}
+                        type="text"
+                        id="username"
+                        name="username"
+                        {...register("username", {
+                            required: {
+                                value : true,
+                                message : "Por favor, ingresa el nombre de tu usuario"
+                            },
+                            minLength : {
+                                value : 6,
+                                message : "Por favor, ingresa un usuario con al menos 6 caracteres"
+                            }
+                        })}
+                    />
+                </label>
+                {errors.password && <p>{errors.password.message}</p>}
+                <label>
+                    Contraseña:
+                    <input 
+                        id="password"
+                        name="password"
+                        type="password"
+                        {...register("password", {
+                            required:{
+                                value: true,
+                                message : "Por favor, ingresa una contraseña"
+                            },
+                            minLength : {
+                                value : 10,
+                                message : "Por favor, ingresa una contraseña con al menos 10 caracteres"
+                            }
+                        })}
+                    />
+                </label>
                 <button type="submit">acceder</button>
             </form>
         </>
