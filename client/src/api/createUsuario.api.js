152fdc732ac5b9c9aba7c57e411cb9fea7cc130e
santiago <santiagodeandrade4@gmail.com>
2023-06-25 18:07:57 -0400

    Arreglo de bug de endpoint

--- client/src/api/createUsuario.api.js
+++ client/src/api/createUsuario.api.js
@@ -3,7 +3,7 @@ import { BACKEND_URL } from '../main'
 
 export async function createUsuarioAPI(data){
     try {
-        const response = await axios.post(BACKEND_URL+"api/create",data)
+        const response = await axios.post(BACKEND_URL+"api/create/",data)
         return response
     } catch(error){
         console.log('Error al crear usuario ')
