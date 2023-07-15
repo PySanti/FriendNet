8f14052923b4b0f15e6ce47b8f74a723698a1b28
santiago <santiagodeandrade4@gmail.com>
2023-07-15 09:45:45 -0400

    Refactor de Notifications.jsx

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -4,17 +4,19 @@
  * @param {Function} onNotificationClick funcion que se ejecutara cuando se clickee una notificacion
  */
 export function Notifications({notificationList, onNotificationClick}){
-    let compList = undefined
-    if (notificationList){
-        compList = notificationList.map((notification)=>{
-            return <button onClick={()=>onNotificationClick(notification.code, notification.id)} key={notification.code}>{notification.msg}</button>
-        })
+    const formatingFunction =(notification)=>{
+        return <button className="notification"onClick={()=>onNotificationClick(notification.code, notification.id)} key={notification.code}>{notification.msg}</button>
     }
     return (
         <div className="notifications-container">
-            <h2>Notificaciones</h2>
-            {notificationList && <div className="notification-list">{compList}</div>}
-            {!notificationList && <div className="notification-list"><h5>No tienes notificaciones</h5></div>}
+            <h2 className="notifications-title">Notificaciones</h2>
+            <div className="notification-list">
+                {notificationList ?
+                    notificationList.map(formatingFunction)
+                    :
+                    <h5 className="no-notifications">No tienes notificaciones</h5>
+                }
+            </div>
         </div>
     )
 }
