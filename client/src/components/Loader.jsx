dc1e445b344003ed99d7ac50735c4cddb9869f88
santiago <santiagodeandrade4@gmail.com>
2023-07-15 09:25:25 -0400

    Refactor del Loader.jsx

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -4,15 +4,19 @@ import "../styles/Loader.css"
  * @param {String} state estado de llamada (failed, loading, success)
  */
 export function Loader({state}){
-        /**
-     * Recordar que el input-error siempre debe estar renderizado aun si no
+    /**
+     * Recordar que el Loader siempre debe estar renderizado aun si no
      * tiene contenido, para que cuando lo empiece a tener, no se vea una 
      * modificacion de espacio en el DOM
      */
+    const cls = "state-msg"
     return (
         <div className="state-container">
-            {state &&      <h2 className="state-msg state-activated">{state}</h2>}
-            {!state &&      <h2 className="state-msg"></h2>}
+            {state ?
+                <h2 className={cls+" state-activated"}>{state}</h2>
+                :
+                <h2 className={cls}></h2>
+            }
         </div>
     )
 }
