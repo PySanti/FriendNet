ade77e3a3ff53a59a0a44273a40d741c9f33f23b
santiago <santiagodeandrade4@gmail.com>
2023-07-19 09:01:52 -0400

    Documentacion del UserNotLogged.jsx

--- client/src/pages/UserNotLogged.jsx
+++ client/src/pages/UserNotLogged.jsx
@@ -2,7 +2,12 @@ import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { Header } from "../components/Header"
 import { Button } from "../components/Button"
-import "../styles/UserNotLogged.css"
+
+/**
+ * Componente creado para ser renderizado cuando el usuario
+ * trate de acceder a rutas que necesiten que el usuario
+ * este logeado cuando este no lo esta
+ */
 export function UserNotLogged(){
     const [backToRoot, setBackToRoot] = useState(false)
     const navigate = useNavigate()
