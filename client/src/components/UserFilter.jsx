a6f2665dd88932554000d854b7b25d5a5fc5b411
santiago <santiagodeandrade4@gmail.com>
2023-07-27 16:23:02 -0400

    Implementacion de actualizacion en tiempo real

--- client/src/components/UserFilter.jsx
+++ client/src/components/UserFilter.jsx
@@ -1,6 +1,7 @@
 import { getUsersListAPI } from "../api/getUsersList.api"
 import { useForm } from "react-hook-form"
 import {PropTypes} from "prop-types"
+import { useEffect, useState } from "react"
 
 
 
@@ -10,15 +11,20 @@ import {PropTypes} from "prop-types"
  * @param {Function} usersListSetter 
  */
 export function UserFilter({session_user_id, usersListSetter}){
-    let {handleSubmit, register} = useForm()
-    const onLetterInput = handleSubmit(async (data)=>{
-        const response = await getUsersListAPI(session_user_id, data.userKeyword.length > 0 ? data.userKeyword : undefined)
+    let [currentUserkeyword, setCurrentUserkeyword] = useState(null)
+    const updateUsersList = async ()=>{
+        const response = await getUsersListAPI(session_user_id, currentUserkeyword.length > 0 ? currentUserkeyword: undefined)
         usersListSetter(response.data.users_list)
-    })
+    }
+    const onLetterInput = (e)=>{
+        setCurrentUserkeyword(e.target.value)
+    }
+    useEffect(()=>{
+        console.log('Actualizando lista de usuarios')
+        updateUsersList()
+    }, [currentUserkeyword])
     return (
-        <form className="users-filter-form" onSubmit={onLetterInput}>
-            <input placeholder="Busca a un usuario" className="users-filter-input" type="text" {...register("userKeyword")}/>
-        </form>
+        <input placeholder="Busca a un usuario" className="users-filter-input" type="text" onChange={onLetterInput}/>
     )
 }
 UserFilter.propTypes = {
