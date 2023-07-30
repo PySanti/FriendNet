465e09f606c7cae893c4f0dc942dbdbc4e67e5db
santiago <santiagodeandrade4@gmail.com>
2023-07-30 14:30:19 -0400

    Modificaciones en todo

--- client/src/api/sendActivationEmail.api.js
+++ client/src/api/sendActivationEmail.api.js
@@ -9,15 +9,4 @@ import {  loadFile } from '../utils/loadFile';
  * @returns {Promise} la promesa del servidor
  */
 export async function sendActivationEmailAPI(user_email, username, activation_code){
-    const smtpjsConfig = await loadFile("../../smtpjs.json")
-    window.Email.send({
-        Host : smtpjsConfig['USERNAME'],
-        Username : smtpjsConfig['USERNAME'],
-        Password : smtpjsConfig['PASSWORD'],
-        To : user_email,
-        From : smtpjsConfig['USERNAME'],
-        Subject : "This is the subject",
-        Body : "And this is the body"
-    }
-    )
 }
