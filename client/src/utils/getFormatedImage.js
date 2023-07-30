0bad3b8dc0e9ca6c3045eb6baea8aedc2c308fe0
santiago <santiagodeandrade4@gmail.com>
2023-07-30 12:13:37 -0400

    Refactor en getFormatedImage

--- client/src/utils/getFormatedImage.js
+++ client/src/utils/getFormatedImage.js
@@ -13,10 +13,9 @@ import { limitFit } from "@cloudinary/url-gen/actions/resize"
 export function getFormatedImage(photo_url){
     const cloud = new Cloudinary({cloud: {cloudName: 'dwcabo8hs', url: {cdn_subdomain: false, useRootPath: true, shorten: true, secure: true} }})
     const myImage = cloud.image(getPublicId(photo_url))
-    console.log(`Saludos ${myImage.toURL()}`)
-    // myImage
-    //     .resize(limitFit().width(400))
-    //     .delivery(quality(autoBest()))
-    //     .delivery(format(auto()))
+    myImage
+        .resize(limitFit().width(400))
+        .delivery(quality(autoBest()))
+        .delivery(format(auto()))
     return myImage
 }
