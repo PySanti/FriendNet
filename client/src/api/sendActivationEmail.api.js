bb85c14a4eb4c89c212a9e3f56f979a2ad91e1c8
santiago <santiagodeandrade4@gmail.com>
2023-07-30 17:04:16 -0400

    Pequenio refactor

--- client/src/api/sendActivationEmail.api.js
+++ client/src/api/sendActivationEmail.api.js
@@ -1,4 +1,3 @@
-import {  loadFile } from '../utils/loadFile';
 
 
 /**
--- client/src/utils/loadFile.js
+++
@@ -1,9 +0,0 @@
-/**
- * Carga los datos del archivo file_path y los retorna
- * @param {String} secrets_filepath ruta de archivo a leer
- */
-export async function loadFile(file_path){
-    const loadedFileData = await fetch(file_path)
-    const parsedFileData = await loadedFileData.json()
-    return parsedFileData
-}
