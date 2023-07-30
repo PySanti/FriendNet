a5a7a99f221c400c88bcc2f453cde30fa8abcb80
santiago <santiagodeandrade4@gmail.com>
2023-07-30 18:34:49 -0400

    Eliminacion de funciones inutiles

--- client/src/utils/isFormatedImage.js
+++
@@ -1,9 +0,0 @@
-/**
- * Retorna true en caso de que la cadena pasada sea un link
- * @param image sera una imagen que podria ser o null, o un archivo o una imagen formateada
- */
-export function isFormatedImage(image){
-    const f = (image) && (typeof image === "object") && (image.cloudName)
-    console.log(f ? 'Es una imagen formateada' : "Es una imagen normal")
-    return f
-}
