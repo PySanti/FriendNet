94545d1c4d7f89673d21479fc485f2bc4248dc50
santiago <santiagodeandrade4@gmail.com>
2023-07-30 12:55:43 -0400

    Refactor del save_image_cloudinary : ahora solo pueden haber uploads signed

--- applications/Usuarios/api/save_image_cloudinary.py
+++ applications/Usuarios/api/save_image_cloudinary.py
@@ -2,34 +2,40 @@ import cloudinary
 import cloudinary.uploader
 from ..utils.load_cloudinary_secrets import load_cloudinary_secrets
 
+secrets = load_cloudinary_secrets()
+cloudinary.config(
+    cloud_name  =   secrets["CLOUDINARY__CLOUD_NAME"],
+    api_key     =   secrets["CLOUDINARY__API_KEY"],
+    api_secret  =   secrets["CLOUDINARY__API_SECRET"]
+)
 
+signature = cloudinary.utils.api_sign_request(
+    {"upload_preset": secrets["CLOUDINARY__UPLOAD_PRESET"]}, 
+    secrets["CLOUDINARY__API_SECRET"]
+)
 
+QUALITY_PARAMS = {
+    'quality': 'auto',  # Ajusta la calidad de la imagen al nivel óptimo para su tamaño y contenido
+    'crop': 'limit',  # Recorta la imagen para ajustarla a los límites de tamaño especificados
+}
 def save_image_cloudinary(image, overwriting=False, current_publicid=None ):
     """
         Almacena la imagen en cloudinary o la sobreescribe y retorna la url de la misma
     """
-    secrets = load_cloudinary_secrets()
-    QUALITY_PARAMS = {
-        'quality': 'auto',  # Ajusta la calidad de la imagen al nivel óptimo para su tamaño y contenido
-        'crop': 'limit',  # Recorta la imagen para ajustarla a los límites de tamaño especificados
-    }
+
     if not overwriting:
         response = cloudinary.uploader.upload(
             image, 
-            api_key     =   secrets["CLOUDINARY__API_KEY"],
-            api_secret  =   secrets["CLOUDINARY__API_SECRET"],
-            cloud_name  =   secrets["CLOUDINARY__CLOUD_NAME"],
-            quality     =   QUALITY_PARAMS['quality'],
-            crop        =   QUALITY_PARAMS['crop'],
+            quality         =   QUALITY_PARAMS['quality'],
+            crop            =   QUALITY_PARAMS['crop'],
+            signature = signature
         )
     else:
         response = cloudinary.uploader.upload(
             image, 
-            api_key     =   secrets["CLOUDINARY__API_KEY"],
-            api_secret  =   secrets["CLOUDINARY__API_SECRET"],
-            cloud_name  =   secrets["CLOUDINARY__CLOUD_NAME"], 
             public_id   =   current_publicid,
             overwrite   =   True,
+            signature = signature,
             # optimization
             quality     =   QUALITY_PARAMS['quality'],
             crop        =   QUALITY_PARAMS['crop'],
