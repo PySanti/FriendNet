916176e25cec38663e962d79bdf6ff1adafd9388
santiago <santiagodeandrade4@gmail.com>
2023-07-30 13:22:31 -0400

    Eliminacion de implementacion de emailjs

--- client/src/api/sendActivationEmail.api.js
+++ client/src/api/sendActivationEmail.api.js
@@ -1,21 +1,12 @@
-import emailjs from '@emailjs/browser';
 import {  loadFile } from '../tools/loadFile';
 
 
 /**
- * Carga datos del archivo emailjs.json y envia correo con activation_code al usuario username 
+ * Carga datos del archivo smtpjs.json y envia correo con activation_code al usuario username 
  * @param {String} user_email  
  * @param {String} username  
  * @param {String} activation_code  
  * @returns {Promise} la promesa del servidor
  */
 export async function sendActivationEmailAPI(user_email, username, activation_code){
-    const emailjsSettings   =  await loadFile("../../emailjs.json")
-    emailjs.init(emailjsSettings['EMAILJS_USER_ID']);
-    const data = {
-        user_email : user_email,
-        username : username,
-        activation_code : activation_code
-    }
-    return await emailjs.send(emailjsSettings['EMAILJS_SERVICE_ID'], emailjsSettings['EMAILJS_TEMPLATE_ID'], data)
 }
