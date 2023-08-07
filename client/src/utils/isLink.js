763433c161c0e8719b4bb1904478c69a47604585
santiago <santiagodeandrade4@gmail.com>
2023-08-07 07:19:20 -0400

    Eliminacion de funcion inutil + modificaciones en todo

--- client/src/utils/isLink.js
+++
@@ -1,7 +0,0 @@
-/**
- * Retorna true en caso de que str sea un link
- * @param {String} str 
- */
-export function isLink(str){
-    return (str) && (typeof str === "string") && (str.split('/')[0].includes('http'))
-}
