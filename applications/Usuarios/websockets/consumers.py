0f12bd4908480c1214597a0ac9b468ea075526cd
santiago <santiagodeandrade4@gmail.com>
2023-09-13 08:34:23 -0400

    Implementacion mas avanzada de channel layers

--- applications/Usuarios/websockets/consumers.py
+++ applications/Usuarios/websockets/consumers.py
@@ -1,7 +1,14 @@
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+
 
 class MyConsumer(WebsocketConsumer):
     def connect(self):
+        self.chat_name = 'test'
+        async_to_sync(self.channel_layer.group_add)(
+            self.chat_name,
+            self.channel_name
+        )
         self.accept()
         self.send(text_data="maderfoker")
 
@@ -11,4 +18,15 @@ class MyConsumer(WebsocketConsumer):
     def receive(self, text_data):
         print('RECIBIENDOOOO')
         self.send(text_data)
+        async_to_sync(self.channel_layer.group_send)(
+            self.chat_name,
+            {
+                'type' : 'chat_message',
+                'message' : text_data
+            }
+        )
         pass
+
+    def chat_message(self, event):
+        message = event['message']
+        self.send(text_data=message)
