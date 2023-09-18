7323bfdf98fb30b69d03a5e381464bb65b4db1a1
santiago <santiagodeandrade4@gmail.com>
2023-09-18 19:17:48 -0400

    Solucion a pequenio error en la funcion getRelatedNotifications

--- client/src/utils/getRelatedNotification.js
+++ client/src/utils/getRelatedNotification.js
@@ -5,7 +5,7 @@
  * @param {Array} notificationsList array de notificaciones
  */
 export function getRelatedNotification(clickedUserId, notificationsList){
-    for (let i =0; i <= notificationsList.length ; i++){
+    for (let i =0; i < notificationsList.length ; i++){
         if (notificationsList[i].sender_user.id == clickedUserId){
             return notificationsList[i]
         }
