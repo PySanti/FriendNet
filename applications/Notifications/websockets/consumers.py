802f714f69af3e3bc2b12ebc4b87c8f4976228ff
santiago <santiagodeandrade4@gmail.com>
2023-09-19 08:07:38 -0400

    Implementacion de descarte de grupo cuando se desconecta el websocket de notificacion

--- applications/Notifications/websockets/consumers.py
+++ applications/Notifications/websockets/consumers.py
@@ -2,14 +2,16 @@ from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 from applications.Chats.websockets.ws_utils.print_pretty_groups import print_pretty_groups
 import json
+from applications.Chats.websockets.ws_utils.discard_channel_if_found import discard_channel_if_found
+
 class NotificationsConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
-        
         print(f'-> Estableciendo conexion con websocket de notificaciones')
 
     def disconnect(self, close_code):
         print('-> Desconectando websocket de notificacion')
+        discard_channel_if_found(self.channel_layer, self.channel_name)
 
     def receive(self, text_data):
         print('Recibiendo info del front-end')
