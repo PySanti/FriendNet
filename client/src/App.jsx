c3e69da484b8bb10c74f18a3aa193765ddbf685e
santiago <santiagodeandrade4@gmail.com>
2023-09-22 11:42:01 -0400

    Modificacion : ahora no se llama a la api de logoutUser al desmontar el App.jsx, no sirve..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,12 +11,10 @@ import { ChangeEmailForActivation } from "./pages/ChangeEmailForActivation.jsx"
 import {useEffect} from "react"
 import {disconnectWebsocket} from "./utils/disconnectWebsocket"
 import {NOTIFICATIONS_WEBSOCKET} from "./utils/constants"
-import {logoutUser} from "./utils/logoutUser"
 function App() {
   useEffect(()=>{
-    return async ()=>{
+    return ()=>{
       disconnectWebsocket(NOTIFICATIONS_WEBSOCKET)
-      await logoutUser()
     }
   }, [])
   return (
