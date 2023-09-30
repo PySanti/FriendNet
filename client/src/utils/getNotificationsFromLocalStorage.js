8874e54f62bb84e997b5ab998f2614162c09dbbe
santiago <santiagodeandrade4@gmail.com>
2023-09-30 18:18:28 -0400

    modificaciones en todo

--- client/src/utils/getNotificationsFromLocalStorage.js
+++ client/src/utils/getNotificationsFromLocalStorage.js
@@ -2,5 +2,6 @@
  * Retorna las notificaciones almacenadas en el local storage
  */
 export function getNotificationsFromLocalStorage(){
+    console.log('Cargando notificcaciones del localStorage')
     return JSON.parse(localStorage.getItem('notifications'))
 }
