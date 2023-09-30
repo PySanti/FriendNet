06f7fe35c4f589799cdaabbdaf0529c8a3ad9ba8
santiago <santiagodeandrade4@gmail.com>
2023-09-30 11:36:02 -0400

    Creacion de funcion broadcast_connection_inform para modularizacion

--- applications/Notifications/websockets/consumers.py
+++ applications/Notifications/websockets/consumers.py
@@ -3,14 +3,9 @@ from asgiref.sync import async_to_sync
 from applications.Chats.websockets.ws_utils.print_pretty_groups import print_pretty_groups
 import json
 from applications.Chats.websockets.ws_utils.discard_channel_if_found import discard_channel_if_found
-from .ws_utils.notifications_group_name import notifications_group_name
-from applications.Notifications.models import Notifications
-from applications.Usuarios.utils.constants import USERS_LIST_ATTRS
-from applications.Usuarios.models import Usuarios
-from applications.Chats.websockets.ws_utils.messages_group_name import messages_group_name
 from .ws_utils.get_opened_groups_with_id import get_opened_groups_with_id
 from .ws_utils.connection_inform_dict import connection_inform_dict 
-from .ws_utils.broadcast_notification import broadcast_notification
+from .ws_utils.broadcast_connection_inform import broadcast_connection_inform
 
 class NotificationsWSConsumer(WebsocketConsumer):
     def connect(self):
@@ -20,16 +15,13 @@ class NotificationsWSConsumer(WebsocketConsumer):
     def disconnect(self, close_code):
         print('-> Desconectando websocket de notificacion')
         group_name = discard_channel_if_found(self.channel_layer, self.channel_name)
-        for group in get_opened_groups_with_id(group_name, self.channel_layer.groups):
-            async_to_sync(self.channel_layer.group_send)(group,connection_inform_dict(group_name, False))
+        broadcast_connection_inform(user_id=group_name, connected=False)
 
     def receive(self, text_data):
         data = json.loads(text_data)
         if (data["type"] == "group_creation"):
             async_to_sync(self.channel_layer.group_add)(str(data['name']),self.channel_name)
-            for group in get_opened_groups_with_id(data["name"], self.channel_layer.groups):
-                async_to_sync(self.channel_layer.group_send)(group,connection_inform_dict(data['name'], True))
-
+            broadcast_connection_inform(user_id=data["name"], connected=True)
         print_pretty_groups(self.channel_layer.groups)
 
     def broadcast_notification_handler(self, event):
---
+++ applications/Notifications/websockets/ws_utils/broadcast_connection_inform.py
@@ -0,0 +1,12 @@
+from .get_opened_groups_with_id import get_opened_groups_with_id
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+from .connection_inform_dict import connection_inform_dict
+
+def broadcast_connection_inform(user_id, connected):
+    """
+        Se encargara de realizar el broadcast del connection_inform
+    """
+    channel_layer = get_channel_layer()
+    for group in get_opened_groups_with_id(str(user_id), channel_layer.groups):
+        async_to_sync(channel_layer.group_send)(group,connection_inform_dict(str(user_id), connected=connected))
