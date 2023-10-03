780b2de162a94325694a90bb998a509bc791b955
santiago <santiagodeandrade4@gmail.com>
2023-10-03 06:42:24 -0400

    Creacion de loadingState global

---
+++ client/src/store/loadingStateStore.jsx
@@ -0,0 +1,7 @@
+import create from "zustand"
+
+export const useLoadingState = create((set)=>({
+    loadingState : null,
+    setLoadingState : (newState)=>(set(()=>({loadingState : newState}))),
+    successfullyLoaded : ()=>(set(()=>({loadingState : "success"})))
+}))
