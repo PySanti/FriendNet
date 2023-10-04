030ccf8acf3756cc84a9b87f7de34cbcbbf387fe
santiago <santiagodeandrade4@gmail.com>
2023-10-04 11:18:44 -0400

    Modificaciones en todo

--- client/src/store/chatGlobeListStore.jsx
+++ client/src/store/chatGlobeListStore.jsx
@@ -1,4 +1,5 @@
 import create from "zustand"
+
 export const useChatGlobeList = create((set)=>({
     chatGlobeList : [],
     setChatGlobeList : (newChatGlobeList)=>(set(()=>({chatGlobeList : newChatGlobeList})))
