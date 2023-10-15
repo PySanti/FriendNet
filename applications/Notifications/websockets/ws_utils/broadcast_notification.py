b9f99a396f51057d2ec6db9955187130424572c8
santiago <santiagodeandrade4@gmail.com>
2023-10-15 14:34:15 -0400

    Adicion de documentacion al broadcast_notifications

--- applications/Notifications/websockets/ws_utils/broadcast_notification.py
+++ applications/Notifications/websockets/ws_utils/broadcast_notification.py
@@ -2,6 +2,13 @@ from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 
 def broadcast_notification(receiver_id, newNotification):
+    """
+        Se encargara de hacer el broadcast de la newNotification al
+        websocket del receiver_id
+
+        Debe hacerse una comprobacion previa a su uso, para saber si el receiver
+        tiene un channel abierto
+    """
     print(f'-> Haciendo broadcats de notificacion {newNotification["id"]}')
     async_to_sync(get_channel_layer().group_send)(str(receiver_id),
     {    
