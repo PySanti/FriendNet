084778ba0e9a931811a10439cc63a3a829d2e1f2
santiago <santiagodeandrade4@gmail.com>
2023-10-15 09:22:18 -0400

    Modificacion en docs del print_pretty_groups Modificaciones en todo

--- applications/Chats/websockets/ws_utils/print_pretty_groups.py
+++ applications/Chats/websockets/ws_utils/print_pretty_groups.py
@@ -1,7 +1,9 @@
 from channels.layers import get_channel_layer
 def print_pretty_groups():
-    print("~~~~~~~~~")
+    """
+        Imprimira los grupos actuales almacenados en el channel layer
+    """
+    print(" ------------------------------------------------ ")
     print('Grupos')
     for k,v in get_channel_layer().groups.items():
         print(f'{k} -> {v}')
-    print("~~~~~~~~~")
