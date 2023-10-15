235936edfbcd4260e851394625f8662b51a54c17
santiago <santiagodeandrade4@gmail.com>
2023-10-15 07:34:46 -0400

    Testing de la funcion brodcast_dict

--- applications/Chats/websockets/ws_utils/broadcast_dict.py
+++ applications/Chats/websockets/ws_utils/broadcast_dict.py
@@ -1,5 +1,7 @@
 def broadcast_dict(broadcast_type, broadcast_value=None):
     """
         Retornara un diccionario para envio de mensajes a traves de websockets
+
+        Funcion creada para que todos los mensajes tengan el mismo formato
     """
     return {"type" : broadcast_type,"value" : broadcast_value} if broadcast_value else {"type" : broadcast_type}
