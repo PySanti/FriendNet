f99d2d509524aa7fb698667a45ea095bf1510ea1
santiago <santiagodeandrade4@gmail.com>
2023-10-28 19:50:54 -0400

    Cambios realizados en handleStandard

--- client/src/utils/handleStandardApiErrors.js
+++ client/src/utils/handleStandardApiErrors.js
@@ -5,16 +5,15 @@ import {BASE_RATE_LIMIT_BLOCK_RESPONSE, BASE_FALLEN_SERVER_ERROR_MSG, BASE_UNEXP
  * en el loadingStateSetter para DRY
  */
 export function handleStandardApiErrors(response, loadingStateSetter, unexpectedErrorMsg){
-    if (response == undefined || response == BASE_FALLEN_SERVER_ERROR_MSG){
+    console.log('Recibiendo error al handleStandard')
+    console.log(response)
+    if (response == undefined){
         loadingStateSetter(BASE_FALLEN_SERVER_LOG)
     } else {
-        if (response.status == 403){
+        if (response == BASE_FALLEN_SERVER_ERROR_MSG || response == BASE_UNEXPECTED_ERROR_MESSAGE){
+            loadingStateSetter(response == BASE_FALLEN_SERVER_ERROR_MSG ? BASE_FALLEN_SERVER_LOG : BASE_UNEXPECTED_ERROR_LOG)
+        } else if (response.status == 403){
             loadingStateSetter(BASE_RATE_LIMIT_BLOCK_RESPONSE)
-        } else if (response == BASE_FALLEN_SERVER_ERROR_MSG || response == BASE_UNEXPECTED_ERROR_MESSAGE){
-            loadingStateSetter({
-                BASE_FALLEN_SERVER_ERROR_MSG : BASE_FALLEN_SERVER_LOG,
-                BASE_UNEXPECTED_ERROR_MESSAGE : BASE_UNEXPECTED_ERROR_LOG
-            }[response])
         } else {
             loadingStateSetter(unexpectedErrorMsg)
         }
