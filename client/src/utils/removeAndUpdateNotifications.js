ad3d6bf3f0e120155c34ebecc69933b1bd4acd8d
santiago <santiagodeandrade4@gmail.com>
2023-10-28 15:34:00 -0400

    Adicion de docs al removeAndUpdateNotifications

--- client/src/utils/removeAndUpdateNotifications.js
+++ client/src/utils/removeAndUpdateNotifications.js
@@ -1,6 +1,10 @@
 import {removeNotificationFromLocalStorage} from "./removeNotificationFromLocalStorage"
 import {saveNotificationsInLocalStorage} from "./saveNotificationsInLocalStorage"
 
+/**
+ * Esta funcion se encargara de eliminar la notification del localStorage y guardar
+ * las notificaciones actualizadas en el mismo localStorage y en el notificationsState
+ */
 export function removeAndUpdateNotifications(notification, notificationsSetter){
     const updatedNotifications = removeNotificationFromLocalStorage(notification)
     saveNotificationsInLocalStorage(updatedNotifications)
