994617ac2c9d530e9e01cd6bb5220cf48b8367bb
santiago <santiagodeandrade4@gmail.com>
2023-10-30 08:51:31 -0400

    Implementacion de prop msg en el UserNotLogged page

--- client/src/pages/UserNotLogged.jsx
+++ client/src/pages/UserNotLogged.jsx
@@ -1,20 +1,28 @@
 import { useNavigate } from "react-router-dom"
 import { Header } from "../components/Header"
 import { Button } from "../components/Button"
-
+import {PropTypes} from "prop-types"
 
 /**
  * Page a renderizar cuando el usuario trate de acceder a una pagina a la que debe
  * estar logeado sin estarlo
+ * @param {String} msg mensaje de error para mostrar al usuario
  */
-export function UserNotLogged(){
+export function UserNotLogged({msg}){
     const navigate = useNavigate()
     return (
         <div className="centered-container">
             <div className="usernot-logged-container">
-                <Header msg="Aun no estas logeado"/>
+                <Header msg={msg ? msg : "Aun no estas logeado"}/>
                 <Button buttonText="Logearme" onClickFunction={()=> {navigate('/')}}/>
             </div>
         </div>
     )
+}
+
+UserNotLogged.propTypes = {
+    msg : PropTypes.string
+}
+UserNotLogged.defaultProps = {
+    msg : undefined
 }
