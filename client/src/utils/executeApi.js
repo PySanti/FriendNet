67eeab06835266a78585bc2dccff109a295d1b47
santiago <santiagodeandrade4@gmail.com>
2023-11-03 11:33:56 -0400

    Modificacion de definicion de executeApi para hacerla mas usable en casos mas generales y para poder manejar mejor los errores estandar

--- client/src/utils/executeApi.js
+++ client/src/utils/executeApi.js
@@ -1,8 +1,8 @@
 import {redirectExpiredUser} from "../utils/redirectExpiredUser"
 import {getJWTFromLocalStorage} from "../utils/getJWTFromLocalStorage"
 import {refreshTokenAPI} from "../api/refreshToken.api"
-import {BASE_FALLEN_SERVER_ERROR_MSG, BASE_UNEXPECTED_ERROR_MESSAGE, UNAUTHORIZED_STATUS_CODE, JWT_LOCALSTORAGE_NAME, BASE_LOGIN_REQUIRED_ERROR_MSG} from "../utils/constants"
-
+import {JWT_LOCALSTORAGE_NAME} from "../utils/constants"
+import {handleStandardApiErrors} from "./handleStandardApiErrors"
 
 /**
  * Funcion creada para estandarizar el protocolo de ejecucion de tanto api's normales
@@ -17,22 +17,20 @@ export async function executeApi(apiCallingFunction, navigateFunc, loadingStateS
             response = await apiCallingFunction() 
             break
         } catch(error){
-            if (error.message == BASE_FALLEN_SERVER_ERROR_MSG || error.response.status !== 401){
-                return error.message == BASE_FALLEN_SERVER_ERROR_MSG ? BASE_FALLEN_SERVER_ERROR_MSG :  error.response
-            } else if (error.response.status === 401){ // error por token
-                try {
-                    const response = await refreshTokenAPI(getJWTFromLocalStorage().refresh)
-                    localStorage.setItem(JWT_LOCALSTORAGE_NAME, JSON.stringify(response.data))
-                } catch(error) {
-                    if (error.response.status === UNAUTHORIZED_STATUS_CODE){
+            if (!handleStandardApiErrors(response, loadingStateSetter)){
+                if (error.response.status === 401){ // error por token
+                    try {
+                        const response = await refreshTokenAPI(getJWTFromLocalStorage().refresh)
+                        localStorage.setItem(JWT_LOCALSTORAGE_NAME, JSON.stringify(response.data))
+                    } catch(error) {
                         redirectExpiredUser(navigateFunc)
                         return undefined
-                    } else {
-                        return error.message == BASE_FALLEN_SERVER_ERROR_MSG ? BASE_FALLEN_SERVER_ERROR_MSG : BASE_UNEXPECTED_ERROR_MESSAGE 
                     }
+                } else {
+                    return error.response
                 }
             } else {
-                return BASE_UNEXPECTED_ERROR_MESSAGE
+                return true
             }
         }
     }
