6dfe569937d651276c787b00bd3334bb2e43968f
santiago <santiagodeandrade4@gmail.com>
2023-11-04 19:22:40 -0400

    Modificacion de la definicion de handleStandardErrorsApi

--- client/src/utils/handleStandardApiErrors.js
+++ client/src/utils/handleStandardApiErrors.js
@@ -6,10 +6,9 @@ import {BASE_FALLEN_SERVER_ERROR_MSG, BASE_FALLEN_SERVER_LOG, BASE_RATE_LIMIT_BL
  * 
  * Retornara true en caso de que se logre manejar el error, false en caso contrario
  */
-export function handleStandardApiErrors(response, loadingStateSetter){
-    const is_fallen_server = response === BASE_FALLEN_SERVER_ERROR_MSG || response.message === BASE_FALLEN_SERVER_ERROR_MSG 
-    if (is_fallen_server || response.status == 403){
-        loadingStateSetter(is_fallen_server ? BASE_FALLEN_SERVER_LOG : BASE_RATE_LIMIT_BLOCK_RESPONSE)
+export function handleStandardApiErrors(error, loadingStateSetter){
+    if (error.message === BASE_FALLEN_SERVER_ERROR_MSG  || error.response.status == 403){
+        loadingStateSetter(error.message === BASE_FALLEN_SERVER_ERROR_MSG ? BASE_FALLEN_SERVER_LOG : BASE_RATE_LIMIT_BLOCK_RESPONSE)
         return true;
     } else {
         return false;
