a5ed3067342f1aec974faa7857ac4a593b3246a1
santiago <santiagodeandrade4@gmail.com>
2023-11-05 21:20:15 -0400

    Creacion e implementacion del NotificationsWSSetPriorityMsgReception

--- client/src/utils/NotificationsWSInitialize.js
+++ client/src/utils/NotificationsWSInitialize.js
@@ -1,6 +1,7 @@
 import {NOTIFICATIONS_WEBSOCKET} from "../utils/constants"
 import {NOTIFICATIONS_WEBSOCKET_ENDPOINT} from "../utils/constants"
 import {NotificationsWSGroupCreationMsg} from "../utils/NotificationsWSGroupCreationMsg"
+import {NotificationsWSSetPriorityMsgReception} from "./NotificationsWSSetPriorityMsgReception"
 /**
  * Inicializa el websocket de notificaciones
  */
@@ -9,12 +10,6 @@ export function NotificationsWSInitialize(userId, setNotificationsIdsCached){
     NOTIFICATIONS_WEBSOCKET.current.onopen = ()=>{
         console.log('Estableciendo conexion')
         NOTIFICATIONS_WEBSOCKET.current.send(NotificationsWSGroupCreationMsg(userId))
-        NOTIFICATIONS_WEBSOCKET.current.onmessage = (event)=>{
-            const data = JSON.parse(event.data)
-            console.log(data)
-            if (data.type === "notifications_ids_cached_inform"){
-                setNotificationsIdsCached(true)
-            }
-            }
-        }
+        NotificationsWSSetPriorityMsgReception(setNotificationsIdsCached)
+    }
 }
