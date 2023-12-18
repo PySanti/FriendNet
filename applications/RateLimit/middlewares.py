9a48291354673bb4b5ed2eb3a5faeb61e8ff015d
santiago <santiagodeandrade4@gmail.com>
2023-12-18 08:57:58 -0400

    Avances realizados en RateLimitMiddleware

--- applications/RateLimit/middlewares.py
+++ applications/RateLimit/middlewares.py
@@ -2,10 +2,17 @@ from django.utils.deprecation import MiddlewareMixin
 from django.http import JsonResponse
 from rest_framework import status
 from .models import RateLimitInfo
+from datetime import datetime
+import pytz
 
 class RateLimitMiddleware(MiddlewareMixin):
     def process_request(self, request):
         client_ip = request.META.get('REMOTE_ADDR')
         client = RateLimitInfo.objects.get_or_create(ip=client_ip)[0]
-        print(client.last_cut_time)
+        last_cut_timediff = int((datetime.now(pytz.timezone('UTC')) - client.last_cut_time).seconds)
+        if (last_cut_timediff > 5):
+            client.last_cut_time = datetime.now()
+            client.save()
+        else:
+            pass
         return JsonResponse({"error" : False}, status=status.HTTP_429_TOO_MANY_REQUESTS)
