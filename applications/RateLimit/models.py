c1f52337b6985569e8f07f6d5e59d1fd7a4e839c
santiago <santiagodeandrade4@gmail.com>
2023-12-18 08:03:41 -0400

    Creacion de modelo RateLimitInfo

--- applications/RateLimit/models.py
+++ applications/RateLimit/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class RateLimitInfo(models.Model):
+    ip = models.CharField(unique=True)
+    banned = models.BooleanField()
+    suspended = models.BooleanField()
+    calls_in_cut = models.PositiveSmallIntegerField()
+    last_cut_time = models.TimeField()
+    
+    def __str__(self):
+        return f"{self.ip} {'banned' if self.banned else 'suspended' if self.suspended else ''}"
