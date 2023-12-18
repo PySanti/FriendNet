1abc368438df9a72f347ea457e2e6f39914efac5
santiago <santiagodeandrade4@gmail.com>
2023-12-18 09:26:09 -0400

    Modificaciones en el RateLimitInfo model

--- applications/RateLimit/middlewares.py
+++ applications/RateLimit/middlewares.py
@@ -7,15 +7,16 @@ import pytz
 
 class RateLimitMiddleware(MiddlewareMixin):
     def process_request(self, request):
+        client_ip = request.META.get('REMOTE_ADDR')
+        client = RateLimitInfo.objects.get_or_create(ip=client_ip)[0]
         if client.banned:
             return JsonResponse({"error" : "banned_ip"}, status=status.HTTP_400_BAD_REQUEST)
         elif client.suspended:
             return JsonResponse({"error" : f"suspended ip until"}, status=status.HTTP_400_BAD_REQUEST)
-        client_ip = request.META.get('REMOTE_ADDR')
-        client = RateLimitInfo.objects.get_or_create(ip=client_ip)[0]
-        last_cut_timediff = RateLimitInfo.objects.get_cut_timediff(client)
-        if (last_cut_timediff > 5):
-            RateLimitInfo.objects.update_cut_time(client)
         else:
-            print(last_cut_timediff)
+            last_cut_timediff = RateLimitInfo.objects.get_cut_timediff(client)
+            if (last_cut_timediff > 5):
+                RateLimitInfo.objects.update_cut_time(client)
+            else:
+                print(last_cut_timediff)
         return JsonResponse({"error" : False}, status=status.HTTP_429_TOO_MANY_REQUESTS)
--- applications/RateLimit/models.py
+++ applications/RateLimit/models.py
@@ -5,9 +5,10 @@ from django.db import models
 class RateLimitInfo(models.Model):
     ip = models.CharField(unique=True)
     banned = models.BooleanField(default=False)
-    suspended = models.BooleanField(default=False)
+    suspended = models.DateTimeField(default=None)
     calls_in_cut = models.PositiveSmallIntegerField(default=0)
     last_cut_time = models.DateTimeField(auto_now_add=True)
+    strikes = models.PositiveSmallIntegerField(default=0)
     objects = RateLimitInfoManager()
     def __str__(self):
         return f"{self.ip} {'banned' if self.banned else 'suspended' if self.suspended else ''}"
