06d9e21ab5df56bad47110d3da41c9c2ed4f0e41
santiago <santiagodeandrade4@gmail.com>
2023-12-21 16:25:06 -0400

    Pequenios cambios realizados a la RateLimit

--- applications/RateLimit/middlewares.py
+++ applications/RateLimit/middlewares.py
@@ -9,11 +9,10 @@ class RateLimitMiddleware(MiddlewareMixin):
         client_ip = request.META.get('REMOTE_ADDR')
         client = RateLimitInfo.objects.get_or_create(ip=client_ip)[0]
         if client.banned:
-            return JsonResponse({"error" : "banned_ip_fucK_u"}, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse({"error" : "banned_ip_fucK_u"}, status=status.HTTP_403_FORBIDDEN)
         if RateLimitInfo.objects.client_is_suspended(client):
-            return JsonResponse({"error" : f"suspended ip until {client.suspended_time}"}, status=status.HTTP_400_BAD_REQUEST)
-        last_cut_timediff = RateLimitInfo.objects.get_cut_timediff(client)
-        if (last_cut_timediff > 5):
+            return JsonResponse({"error" : f"suspended ip until {client.suspended_time}"}, status=status.HTTP_403_FORBIDDEN)
+        if (RateLimitInfo.objects.get_cut_timediff(client) > 5):
             RateLimitInfo.objects.update_cut(client)
         else:
             RateLimitInfo.objects.update_calls_in_cut(client)
