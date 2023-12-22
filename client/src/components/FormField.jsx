3c073437bc2c62841760a48567082e6512ecb1df
santiago <santiagodeandrade4@gmail.com>
2023-12-21 20:48:01 -0400

    Eliminacion de componente Label y de prop label en el FormField

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,19 +1,16 @@
 import { InputError } from "./InputError";
 import {PropTypes} from "prop-types"
-import { Label } from "./Label";
 import "../styles/FormField.css"
 /**
  * Componente creado para simplficar codigo de campos de Input. Wrapper de InputError, Label e inputs en si
  * @param {String} errors errores producidos por hook de manejo de errores de formulario
- * @param {String} label etiqueta de input
  * @param {ReactElement} children 
  */
-export function FormField({errors, label, children}){
+export function FormField({errors,  children}){
     return (
         <div className="form-field-container">
             <InputError msg={errors}/>
             <div className="input-pair-container">
-                {label && <Label msg={label}/>}
                 {children}
             </div>
         </div>
@@ -22,12 +19,10 @@ export function FormField({errors, label, children}){
 FormField.propTypes = {
     children : PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
     errors : PropTypes.string,
-    label : PropTypes.string,
 }
 
 FormField.defaultProps = {
     errors : undefined,
-    label : undefined,
 }
 
 
