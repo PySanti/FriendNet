09231da16934a4902446119ebbc8092bac9e35a7
santiago <santiagodeandrade4@gmail.com>
2023-12-22 10:26:18 -0400

    Implementacion de logica para animacion de previsualization usando Lottie

--- client/src/components/PasswordField.jsx
+++ client/src/components/PasswordField.jsx
@@ -1,7 +1,9 @@
 import { FormField } from "./FormField";
 import {PropTypes} from "prop-types"
 import "../styles/PasswordField.css"
-import { useState } from "react";
+import { useState, useRef } from "react";
+import eye from "../../lottie/eye"
+import Lottie from "lottie-react"
 
 /**
  * Componente creado para campos de contrasenia
@@ -11,6 +13,12 @@ import { useState } from "react";
  */
 export function PasswordField({errors, registerObject, label}){
     let [previsualizationActivated, setPrevisualizationActivated] = useState(false)
+    const eyeAnimationRef = useRef()
+    const handleEyeClick = ()=>{
+        setPrevisualizationActivated(!previsualizationActivated)
+        eyeAnimationRef.current.setSpeed(3)
+        eyeAnimationRef.current.playSegments(!previsualizationActivated ? [0,60] : [60,0], true)
+    }
     return (
         <FormField  errors={errors}>
             <input 
@@ -19,8 +27,13 @@ export function PasswordField({errors, registerObject, label}){
                 type={previsualizationActivated ? "text" : "password"} 
                 name={registerObject.name} 
                 {...registerObject}/>
-            <div className="password-visualization" onClick={()=>setPrevisualizationActivated(!previsualizationActivated)}>
-                <i className="material-symbols-outlined" >{previsualizationActivated ? "Visibility" : "Visibility_Off"}</i>
+            <div className="password-visualization" onClick={handleEyeClick}>
+                <Lottie 
+                    loop={false}
+                    autoplay={false}
+                    animationData={eye} 
+                    lottieRef={eyeAnimationRef} 
+                    />
             </div>
         </FormField>
     )
