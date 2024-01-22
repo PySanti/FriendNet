861a984bcbe3f19fe7801a20db5b1d872062adec
santiago <santiagodeandrade4@gmail.com>
2024-01-22 14:52:36 -0400

    Implementacion de animacion para darkMode con lottie

--- client/src/components/DarkModeButton.jsx
+++ client/src/components/DarkModeButton.jsx
@@ -1,8 +1,26 @@
+import dark_mode from "../../lottie/dark_mode"
+import Lottie from "lottie-react"
+import {useRef} from "react"
+
+
+/**
+ * Componente creado para el dark mode
+ */
 export function DarkModeButton(){
+    const darkModeAnimationRef = useRef()
+    const handleClick = ()=>{
+        document.getElementById("root").classList.toggle("dark")
+        darkModeAnimationRef.current.playSegments(document.getElementById("root").classList.contains("dark") ? [0,16] : [16,0], true)
+    }
     return (
         <div className="dark-mode-button__container">
-            <button className="dark-mode-button" onClick={()=>document.getElementById("root").classList.toggle("dark")}>
-                Dark
+            <button className="dark-mode-button" onClick={handleClick}>
+                <Lottie 
+                    loop={false}
+                    autoplay={false}
+                    animationData={dark_mode} 
+                    lottieRef={darkModeAnimationRef} 
+                    />
             </button>
         </div>
     )
