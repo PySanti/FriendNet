0ccd635a72e189a5cd3b536c569fd077c1304208
santiago <santiagodeandrade4@gmail.com>
2024-01-22 15:35:35 -0400

    Modificacion de --base-background-color por --base-color  y adicion de variable real --base-background-color Modificacion de forma de seleccion del root para dark mode

--- client/src/components/DarkModeButton.jsx
+++ client/src/components/DarkModeButton.jsx
@@ -1,7 +1,7 @@
 import dark_mode from "../../lottie/dark_mode"
 import Lottie from "lottie-react"
 import {useRef} from "react"
-
+import "../styles/DarkModeButton.css"
 
 /**
  * Componente creado para el dark mode
@@ -9,19 +9,19 @@ import {useRef} from "react"
 export function DarkModeButton(){
     const darkModeAnimationRef = useRef()
     const handleClick = ()=>{
-        document.getElementById("root").classList.toggle("dark")
-        darkModeAnimationRef.current.playSegments(document.getElementById("root").classList.contains("dark") ? [0,16] : [16,0], true)
+        const body = document.getElementsByTagName("body")[0]
+        body.classList.toggle("dark")
+        darkModeAnimationRef.current.setSpeed(1.6)
+        darkModeAnimationRef.current.playSegments(body.classList.contains("dark") ? [0,16] : [16,0], true)
     }
     return (
-        <div className="dark-mode-button__container">
-            <button className="dark-mode-button" onClick={handleClick}>
-                <Lottie 
-                    loop={false}
-                    autoplay={false}
-                    animationData={dark_mode} 
-                    lottieRef={darkModeAnimationRef} 
-                    />
-            </button>
+        <div className="dark-mode-button__container" onClick={handleClick}>
+            <Lottie 
+                loop={false}
+                autoplay={false}
+                animationData={dark_mode} 
+                lottieRef={darkModeAnimationRef} 
+                />
         </div>
     )
 }
