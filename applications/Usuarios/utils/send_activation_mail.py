fc479815ac67e2bca3820e9e19ebc4c03a73a55b
santiago <santiagodeandrade4@gmail.com>
2024-01-22 11:05:37 -0400

    Adicion de estilos para envio de correo de activacion

--- applications/Usuarios/utils/send_activation_mail.py
+++ applications/Usuarios/utils/send_activation_mail.py
@@ -1,5 +1,44 @@
 from django.core.mail import send_mail
 
+def activation_mail_html_content(activation_code):
+    return  """
+<html>
+    <head>
+        <style>
+            .title{
+                font-size : 30px;
+            }
+            *{
+                text-align : center;
+                font-family : sans-serif;
+                font-weight : 530;
+                color : #000;
+            }
+            button{
+                font-size: 12px;
+                border: none;
+                border-radius: 0px;
+                border-bottom-left-radius: 10px;
+                border-top-right-radius: 10px;
+                height: auto;
+                width: auto;
+                padding: .4rem;
+                outline: 3px solid #4ed31a;
+                margin-bottom : 30px;
+            }
+
+        </style>
+    </head>
+    <body>
+            <h1 class="title">
+                FriendNet
+            </h1>
+            <button class="content">
+                %s
+            </button>
+    </body>
+</html>""" % activation_code;
+
 def send_activation_mail(email, activation_code):
     """
         Funcion creada para estandarizar envio de mensajes usando brevo
@@ -7,6 +46,7 @@ def send_activation_mail(email, activation_code):
     try:
         send_mail(
             subject         =   "Activa tu cuenta", 
+            html_message    =  activation_mail_html_content(activation_code),
             message         =   f"Codigo : {activation_code}", 
             from_email      =   "friendnetcorp@gmail.com", 
             recipient_list  =   [email])
