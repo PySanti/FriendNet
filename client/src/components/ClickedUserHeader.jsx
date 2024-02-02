3f83baa1e234134aca009bece05400bf92dc8e0f
santiago <santiagodeandrade4@gmail.com>
2024-02-02 09:13:36 -0400

    Mejora de estilos del ClickedUserTyping

--- client/src/components/ClickedUserHeader.jsx
+++ client/src/components/ClickedUserHeader.jsx
@@ -14,7 +14,7 @@ export function ClickedUserHeader(){
             <UserPhoto photoFile={clickedUser.photo_link} chatPhoto/>
             <div className="clicked-user__username-container">
                 <h3 className="clicked-user__username">{clickedUser.username}{clickedUser.is_online && ", en linea"}</h3>
-                <h3 className={typingDB[clickedUser.id]? "clicked-user__typing clicked-user__typing__activated" : "clicked-user__typing"}>{typingDB[clickedUser.id] && "escribiendo ... "}</h3>
+                <h3 className="clicked-user__typing">{typingDB[clickedUser.id] && "escribiendo ... "}</h3>
             </div>
         </div>
     )
