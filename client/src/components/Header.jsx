8c8b70825e5e1ed17fa5a3ffa168adfbfb172654
santiago <santiagodeandrade4@gmail.com>
2024-02-12 10:47:09 -0400

    Modificacion del header para mejor responsive. Implementacion de logo en pestania

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,24 +1,23 @@
 import "../styles/Header.css"
 import {PropTypes} from "prop-types"
 import {getUserDataFromLocalStorage} from "../utils/getUserDataFromLocalStorage"
+import {useDarkMode} from "../store"
 /**
  * Cabecera estandar de la pagina
  * @param {String} msg mensaje a renderizar en conjunto con cabecera
  */
 export function Header({msg}) {
     const userData = getUserDataFromLocalStorage()
-
+    let darkModeActivated  = useDarkMode((state)=>(state.darkModeActivated))
     return (
         <>
             <header className="header-container">
-                <h1 className="header-title-container">
-                    <span className="header-logo-container">
-                        <img className="header-logo" src="/logo3.png"/>
-                    </span>
-                    <span className="header-content-container">
+                <div className="header-title-container">
+                    <h1 className="header-content">
+                        <img className="header-logo" src={!darkModeActivated ? "/logo3V3.png" : "/logo3V3Inverted.png"}/>
                         riendNet{userData && `, ${userData.username}`}
-                    </span>
-                </h1>
+                    </h1>
+                </div>
                 {msg && <h2 className="header-msg">{msg}</h2>}
             </header>
         </>
