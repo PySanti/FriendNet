fec325aa7c5cdd0a269d697dfad1422a8a61be43
santiago <santiagodeandrade4@gmail.com>
2024-02-15 07:06:52 -0400

    Cambio tonto

--- applications/Usuarios/views.py
+++ applications/Usuarios/views.py
@@ -240,6 +240,7 @@ class GenerateSendSecurityCodeAPI(APIView):
             if (Usuarios.objects.user_exists(email=serialized_data.data["user_email"])):
                 try:
                     code = generate_security_code()
+                    print(f"Codigo de seguridad {code}")
                     send_activation_mail(serialized_data.data['user_email'], f"{serialized_data.data['message']} - {code}")
                     Usuarios.objects.set_security_code(Usuarios.objects.get(email=serialized_data.data["user_email"]), code)
                     return Response({"email_sended" : True}, status.HTTP_200_OK)
