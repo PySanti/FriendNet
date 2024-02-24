660950b34f2d7a7e1189ff4c14f9e5645268302d
santiago <santiagodeandrade4@gmail.com>
2024-02-24 10:14:18 -0400

    Adicion de friendnet.online al resto de configs para allowed host

--- FriendNet/settings.py
+++ FriendNet/settings.py
@@ -29,10 +29,9 @@ DEBUG = False
 ALLOWED_HOSTS = [
     'https://friendnet.netlify.app', 'friendnet.online'
 ]
-CORS_ALLOWED_ORIGINS = ('https://friendnet.netlify.app',)
+CORS_ALLOWED_ORIGINS = ('https://friendnet.netlify.app', 'friendnet.online')
 CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = ('https://friendnet.netlify.app',)
-
+CORS_ORIGIN_WHITELIST = ('https://friendnet.netlify.app', 'friendnet.online')
 
 # https settings
 SESSION_COOKIE_SECURE = True
