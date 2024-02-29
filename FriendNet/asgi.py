9f9c28e819c2f18ccc35b9d89394ca5925290855
santiago <santiagodeandrade4@gmail.com>
2024-02-29 10:00:34 -0400

    Modificacion de ASGI settings

--- FriendNet/asgi.py
+++ FriendNet/asgi.py
@@ -13,7 +13,7 @@ from channels.routing import get_default_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", f'FriendNet.settings')
 django.setup()
 
-application = get_default_application()
+# application = get_default_application()
 
 
 application = ProtocolTypeRouter(
