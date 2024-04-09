e1714af5733ac0fae5e13e0fe0bcccc8edd80864
santiago <santiagodeandrade4@gmail.com>
2024-04-09 07:39:25 -0400

    Implementacion de uso de fechas para loggers de websockets

--- applications/Notifications/websockets/ws_utils/print_pretty_groups.py
+++ applications/Notifications/websockets/ws_utils/print_pretty_groups.py
@@ -1,6 +1,7 @@
 from django.core.cache import cache
 import logging
 from .manage_groups import manage_groups
+from datetime import datetime
 from applications.Usuarios.utils.constants import (
     BASE_CHATS_WEBSOCKETS_GROUP_NAME,
     BASE_NOTIFICATIONS_WEBSOCKETS_GROUP_NAME
@@ -11,7 +12,7 @@ def print_pretty_groups():
         Imprimira los grupos actuales almacenados en el channel layer
     """
     logger = logging.getLogger('django.channels')
-    logger.info(" ------------------------------------------------ ")
+    logger.info(f" ------------------------------------------------ {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
     modes = [BASE_NOTIFICATIONS_WEBSOCKETS_GROUP_NAME, BASE_CHATS_WEBSOCKETS_GROUP_NAME]
     for m in modes:
         current_group = manage_groups("get", m)
