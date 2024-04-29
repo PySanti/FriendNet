45b543284761e4abef6776b34bde3853735514dc
santiago <santiagodeandrade4@gmail.com>
2024-04-29 12:23:17 -0400

    Implementacion de logger en middleware de RateLimimt

--- applications/RateLimit/middlewares.py
+++ applications/RateLimit/middlewares.py
@@ -2,15 +2,22 @@ from django.utils.deprecation import MiddlewareMixin
 from django.http import JsonResponse
 from rest_framework import status
 from .models import RateLimitInfo
+import logging
+
+
+logger = logging.getLogger('django.channels')
 
 class RateLimitMiddleware(MiddlewareMixin):
     def process_request(self, request):
         # recordar agregar docs a miro
         client_ip = request.META.get('REMOTE_ADDR')
         client = RateLimitInfo.objects.get_or_create(ip=client_ip)[0]
+        logger.info(f"{client.id} ---- {request.path}")
         if client.banned:
+            logger.info(f"{client.id} ---- banned")
             return JsonResponse({"error" : "banned_ip_fucK_u"}, status=status.HTTP_403_FORBIDDEN)
         elif RateLimitInfo.objects.client_is_suspended(client):
+            logger.info(f"{client.id} ---- suspended")
             return JsonResponse({"error" : f"suspended_ip", "until" : client.suspended_time}, status=status.HTTP_403_FORBIDDEN)
         else:
             if (RateLimitInfo.objects.get_cut_timediff(client) > 5):
