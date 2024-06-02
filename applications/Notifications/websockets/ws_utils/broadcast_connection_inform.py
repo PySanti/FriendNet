7be17abb23b2d1cb785733881231e3deab9f64f1
santiago <santiagodeandrade4@gmail.com>
2024-06-02 14:04:38 -0400

    Modificacion de logging para debug de websockets

--- applications/Notifications/websockets/ws_utils/broadcast_connection_inform.py
+++ applications/Notifications/websockets/ws_utils/broadcast_connection_inform.py
@@ -1,6 +1,11 @@
 from .get_opened_chat_groups_with_id import get_opened_chat_groups_with_id
 from channels.layers import get_channel_layer
 import logging
+from ..ws_utils.manage_groups import manage_groups
+from applications.Usuarios.utils.constants import (
+    BASE_CHATS_WEBSOCKETS_GROUP_NAME,
+    BASE_NOTIFICATIONS_WEBSOCKETS_GROUP_NAME
+)
 
 
 logger = logging.getLogger('django.channels')
@@ -14,7 +19,8 @@ async def broadcast_connection_inform(user_id, connected):
     opened_chat_groups = get_opened_chat_groups_with_id(str(user_id))
     if (opened_chat_groups):
         for group in opened_chat_groups:
-            logger.info(f"Haciendo broadcast a {group}")
+            channels_in = manage_groups("get", BASE_CHATS_WEBSOCKETS_GROUP_NAME)[group]
+            logger.info(f"Haciendo broadcast a {group}, con channels : {channels_in}")
             await channel_layer.group_send(group,{
             'type' : 'broadcast_connection_inform_handler',
             'value' : {
