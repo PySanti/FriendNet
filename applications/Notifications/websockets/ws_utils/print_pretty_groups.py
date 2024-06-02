e897ada757476c3defed219b84b01946e33e8676
santiago <santiagodeandrade4@gmail.com>
2024-06-02 13:50:00 -0400

    Eliminacion de fecha a la hora de imprimir grupos, innecesario

--- applications/Notifications/websockets/ws_utils/print_pretty_groups.py
+++ applications/Notifications/websockets/ws_utils/print_pretty_groups.py
@@ -12,7 +12,6 @@ def print_pretty_groups():
     """
         Imprimira los grupos actuales almacenados en el channel layer
     """
-    logger.info(f" ------------------------------------------------ {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
     modes = [BASE_NOTIFICATIONS_WEBSOCKETS_GROUP_NAME, BASE_CHATS_WEBSOCKETS_GROUP_NAME]
     for m in modes:
         current_group = manage_groups("get", m)
