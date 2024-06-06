8d255c194aa7c072068425171422319df9d5c19b
santiago <santiagodeandrade4@gmail.com>
2024-06-06 13:00:55 -0400

    Mejora de funcion get_redis_groups para pruebas de funcionamiento en produccion:

--- applications/Notifications/websockets/ws_utils/get_redis_groups.py
+++ applications/Notifications/websockets/ws_utils/get_redis_groups.py
@@ -1,8 +1,12 @@
 import redis
-def get_redis_groups():
+def get_redis_groups(key):
     r = redis.Redis(host='localhost', port=6379, db=0)
-    groups = {}
+    groups = {
+        "notifications" : {},
+        "chats" : {}
+    }
     for k in r.keys():
         group_name = k.decode("utf-8").split(":")[-1]       
-        groups[group_name] = [c.decode("utf-8") for c in r.zrange(k, 0 , -1)]
-    return groups
+        group_value = [c.decode("utf-8") for c in r.zrange(k, 0 , -1)]
+        groups["chats" if "-" in group_name else "notifications"][group_name] = group_value
+    return groups[key]
--- applications/Notifications/websockets/ws_utils/print_pretty_groups.py
+++ applications/Notifications/websockets/ws_utils/print_pretty_groups.py
@@ -6,6 +6,7 @@ from applications.Usuarios.utils.constants import (
     BASE_CHATS_WEBSOCKETS_GROUP_NAME,
     BASE_NOTIFICATIONS_WEBSOCKETS_GROUP_NAME
 )
+from .get_redis_groups import get_redis_groups
 
 logger = logging.getLogger('django.channels')
 def print_pretty_groups():
@@ -18,3 +19,7 @@ def print_pretty_groups():
         if current_group:
             for k,v in current_group.items():
                 logger.info(f'{k} -> {v}')
+    
+    logger.info("\t\t\t~~~\tMostrando grupos a partir de redis")
+    logger.info(get_redis_groups("notifications"))
+    logger.info(get_redis_groups("chats"))
