42afcc894c1f24a8bd0ea5b64bf41170a46933dc
santiago <santiagodeandrade4@gmail.com>
2024-06-07 07:29:17 -0400

    Modificacion para produccion
    Pequenio refactor de funcion get_redis_groups

--- applications/Notifications/websockets/ws_utils/get_redis_groups.py
+++ applications/Notifications/websockets/ws_utils/get_redis_groups.py
@@ -1,12 +1,14 @@
 import redis
+
+redis_connection = redis.Redis(host='localhost', port=6379, db=0)
+
 def get_redis_groups(key):
-    r = redis.Redis(host='localhost', port=6379, db=0)
     groups = {
         "notifications" : {},
         "chats" : {}
     }
-    for k in r.keys():
+    for k in redis_connection.keys():
         group_name = k.decode("utf-8").split(":")[-1]       
-        group_value = [c.decode("utf-8") for c in r.zrange(k, 0 , -1)]
+        group_value = [v.decode("utf-8") for v in redis_connection.zrange(k, 0 , -1)]
         groups["chats" if "-" in group_name else "notifications"][group_name] = group_value
     return groups[key]
