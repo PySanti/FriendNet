593da8de05482ea3e434cd52f2738a0c92906101
santiago <santiagodeandrade4@gmail.com>
2024-06-13 19:06:21 -0400

    Solucion a fallas en decode de valores de redis en casos borde

--- applications/Notifications/websockets/ws_utils/get_redis_groups.py
+++ applications/Notifications/websockets/ws_utils/get_redis_groups.py
@@ -1,6 +1,8 @@
 import redis
+import logging
 
 redis_connection = redis.Redis(host='localhost', port=6379, db=0)
+logger_channels = logging.getLogger('django.channels')
 
 def get_redis_groups(key):
     groups = {
@@ -8,7 +10,12 @@ def get_redis_groups(key):
         "chats" : {}
     }
     for k in redis_connection.keys():
-        group_name = k.decode("utf-8").split(":")[-1]       
-        group_value = [v.decode("utf-8") for v in redis_connection.zrange(k, 0 , -1)]
-        groups["chats" if "-" in group_name else "notifications"][group_name] = group_value
+        group_name = k.decode("utf-8").split(":")[-1]
+        group_value = []
+        try:
+            for v in redis_connection.zrange(k, 0 , -1):
+                group_value.append(v.decode("utf-8"))
+            groups["chats" if "-" in group_name else "notifications"][group_name] = group_value
+        except UnicodeDecodeError:
+            logger_channels.info(f"No se puede decodificar el valor: {v}")
     return groups[key]
