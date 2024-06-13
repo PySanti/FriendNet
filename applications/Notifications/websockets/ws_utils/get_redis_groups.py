6e3b6d91cea2d51d810f050451150552c2b34ca8
santiago <santiagodeandrade4@gmail.com>
2024-06-13 19:38:54 -0400

    Refactor en funcion get_redis_groups

--- applications/Notifications/websockets/ws_utils/get_redis_groups.py
+++ applications/Notifications/websockets/ws_utils/get_redis_groups.py
@@ -10,12 +10,10 @@ def get_redis_groups(key):
         "chats" : {}
     }
     for k in redis_connection.keys():
-        group_name = k.decode("utf-8").split(":")[-1]
-        group_value = []
         try:
-            for v in redis_connection.zrange(k, 0 , -1):
-                group_value.append(v.decode("utf-8"))
+            group_name = k.decode("utf-8").split(":")[-1]       
+            group_value = [v.decode("utf-8") for v in redis_connection.zrange(k, 0 , -1)]
             groups["chats" if "-" in group_name else "notifications"][group_name] = group_value
         except UnicodeDecodeError:
-            logger_channels.info(f"No se puede decodificar el valor: {v}")
+            logger_channels.info("Error tratando de decodificar, se omitirá")
     return groups[key]
