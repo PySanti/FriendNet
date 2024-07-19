b80b3595460e70c463c8a4590686c3b76a5ad116
santiago <santiagodeandrade4@gmail.com>
2024-07-19 11:41:51 -0400

    Modificacion para intento de solucion a problema de RateLimit

--- applications/RateLimit/middlewares.py
+++ applications/RateLimit/middlewares.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from rest_framework import status
 from .models import RateLimitInfo
 import logging
+from .utils.valid_ip import valid_ip
 
 
 logger = logging.getLogger('ratelimit_logger')
@@ -11,18 +12,21 @@ class RateLimitMiddleware(MiddlewareMixin):
     def process_request(self, request):
         # recordar agregar docs a miro
         client_ip = request.META.get('REMOTE_ADDR')
-        client = RateLimitInfo.objects.get_or_create(ip=client_ip)[0]
-        logger.warning(f"{client.id} ---- {request.path}")
-        if client.banned:
-            logger.warning(f"{client.id} ---- banned")
-            return JsonResponse({"error" : "banned_ip_fucK_u"}, status=status.HTTP_403_FORBIDDEN)
-        elif RateLimitInfo.objects.client_is_suspended(client):
-            logger.warning(f"{client.id} ---- suspended")
-            return JsonResponse({"error" : f"suspended_ip", "until" : client.suspended_time}, status=status.HTTP_403_FORBIDDEN)
-        else:
-            if (RateLimitInfo.objects.get_cut_timediff(client) > 5):
-                RateLimitInfo.objects.update_cut(client)
+        if valid_ip(client_ip):
+            client = RateLimitInfo.objects.get_or_create(ip=client_ip)[0]
+            logger.warning(f"{client.id} ---- {request.path}")
+            if client.banned:
+                logger.warning(f"{client.id} ---- banned")
+                return JsonResponse({"error" : "banned_ip_fucK_u"}, status=status.HTTP_403_FORBIDDEN)
+            elif RateLimitInfo.objects.client_is_suspended(client):
+                logger.warning(f"{client.id} ---- suspended")
+                return JsonResponse({"error" : f"suspended_ip", "until" : client.suspended_time}, status=status.HTTP_403_FORBIDDEN)
             else:
-                RateLimitInfo.objects.update_calls_in_cut(client)
-                if client.calls_in_cut > 30:
-                    RateLimitInfo.objects.suspend_client(client)
+                if (RateLimitInfo.objects.get_cut_timediff(client) > 5):
+                    RateLimitInfo.objects.update_cut(client)
+                else:
+                    RateLimitInfo.objects.update_calls_in_cut(client)
+                    if client.calls_in_cut > 30:
+                        RateLimitInfo.objects.suspend_client(client)
+        else:
+            return JsonResponse({"error" : "suspicious_ip"}, status=status.HTTP_403_FORBIDDEN)
